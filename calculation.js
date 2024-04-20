461f7924fde7ba00abb1205f509b1703f944fa0a
Maryam Al Harthi <162016592+MKS229@users.noreply.github.com>
2024-04-20 23:24:35 +0400

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -20,11 +20,11 @@ function ready() {
         input.addEventListener('change', quantityChanged)
     }
     // adding elemnt to the cart 
-    /*var addToCartButtons = document.getElementsByClassName('btn btn-sm btn-outline-secondary')
+    var addToCartButtons = document.getElementsByClassName('btn btn-sm btn-outline-secondary')
     for (var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
-    }*/
+    }
     // clear cart items
     document.getElementById('clear-cart')[0].addEventListener('click', clearCart)
 }
@@ -45,88 +45,81 @@ function removeCartItem(event) {
 var totalprice
 let listOfItem = []
 // Add to cart function
-/*function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('card-title')[0].innerText
-    var price = shopItem.getElementsByClassName('card-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('card-img-top')[0].src
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
+function addToCartClicked(event) {
+    var button = event.target;
+    var shopItem = button.parentElement.parentElement;
+    var title = shopItem.querySelector('.card-title').innerText;
+    var price = shopItem.querySelector('.card-price').innerText;
+    var imageSrc = shopItem.querySelector('.card-img-top').src;
+    addItemToCart(title, price, imageSrc);
+    updateCartTotal();
 }
-*/
+
 function addItemToCart(title, price, imageSrc) {
-    let itemtable = document.getElementsByTagName('tbody')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-    for (var i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return
-        }
-    }
-    // the new item in cart
+    var cartItemContainer = document.querySelector('.cart-items'); //new item elemnt
     var cartRowContents = `
     <tr class="cart-row">
         <td><img class="Image" src="${imageSrc}"></td>
         <td class="cart-item-title">${title}</td>
-        <td>${price}</td>
+        <td class="cart-price">${price}</td>
         <td><input class="cart-quantity-input" type="number" value="1" min="1"></td>
         <td><button type="button" class="btn btn-danger">Remove</button></td>
     </tr>
-`;
-    itemtable.innerHTML += cartRowContents
-   // cartItems.appendChild(cartRow)
-   // cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-   // cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    `;
+    cartItemContainer.insertAdjacentHTML('beforeend', cartRowContents);
+    var removeCartItemButtons = document.querySelectorAll('.btn-danger');
+    var lastButton = removeCartItemButtons[removeCartItemButtons.length - 1];
+    lastButton.addEventListener('click', removeCartItem);
+    var lastQuantityInput = document.querySelectorAll('.cart-quantity-input');
+    lastQuantityInput[lastQuantityInput.length - 1].addEventListener('change', quantityChanged);
 }
 
 // clear cart function
 function clearCart(){
-    alert('You are Up to Clear the cart !')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    //cartItems.hasAttribute
-    while (cartItems.hasAttribute()) {
-        cartItems.removeAttribute(cartItems.firstChild)
+    alert('You are Up to Clear the cart !');
+    var cartItems = document.querySelector('.cart-items');
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild);
     }
-    updateCartTotal()
+    updateCartTotal();
 }
 // get the total price
 function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    var Tax = 0
-    var discount = 0
+    var cartItemContainer = document.querySelector('.cart-items');
+    var cartRows = cartItemContainer.querySelectorAll('.cart-row');
+    var total = 0;
+    var Tax = 0;
+    var discount = 0;
     for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText)
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.querySelector('.cart-price');
+        var quantityElement = cartRow.querySelector('.cart-quantity-input');
+        var price = parseFloat(priceElement.innerText.replace('$', ''));
+        var quantity = quantityElement.value;
+        total += price * quantity;
     }
-    // add tax 5%
-    Tax = (total*5)/100 
-    Tax = Math.round(Tax*1000)/1000 //round up to 3 dec point
-    document.getElementById('cart-tax').innerText =  Tax // update
-    // discount
-    var code = document.getElementById('DiscountCode').value
-    discount = discount (total,code)
-    // total
-    total += Tax - discount
-    total = Math.round(total * 1000) / 1000 //round up to 3 dec point
-    document.getElementsByClassName('cart-total')[0].innerText = total// update
+    Tax = (total * 5) / 100;
+    Tax = Math.round(Tax * 1000) / 1000; //round up to 3 decimal plase
+    document.getElementById('cart-tax').innerText = Tax.toFixed(2);
+    var code = document.getElementById('DiscountCode').value;
+    discount = discount(total, code);
+    total += Tax - discount;
+    total = Math.round(total * 1000) / 1000;
+    document.querySelector('.cart-total').innerText = total.toFixed(2);
 }
+
 // discount function
-function discount(total,code){
- var discount = 0
- var codeList = ['M01','mm29','24l','age60']
- for(var i = 0;i<codeList.length; i++){
-    if(codeList[i]==code){
-        discount = total * 0.10 // 10% discont for correct code
-        discount = Math.round(discount * 1000) / 1000
-        return discount
+function discount(total, code){
+    var discount = 0;
+    var codeList = ['M01','mm29','24l','age60'];
+    for(var i = 0; i < codeList.length; i++){
+        if(codeList[i] === code){
+            discount = total * 0.10;
+            discount = Math.round(discount * 1000) / 1000;
+            document.getElementById('cart-discount').innerText = discount.toFixed(2);
+            return discount;
+        }
     }
- document.getElementById('cart-discount').innerText = discount // update
-}}
+    document.getElementById('cart-discount').innerText = discount.toFixed(2);
+    return discount;
+}
