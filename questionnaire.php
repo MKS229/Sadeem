be08ab67beab656db0f2f90532eacde6491f6622
Esraa Alsubhi <162142964+ovzev@users.noreply.github.com>
2024-05-06 20:06:24 +0400

    questionnaire.php

--- questionnaire.php
+++ questionnaire.php
@@ -53,6 +53,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title>Questionnaire</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="styles1.css">
     <style>
         body{
             background-color:  #5985a2;
@@ -97,6 +100,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <img src="logo.png" alt="logo" class="logo">
     <p class="logo">Sadeem</p>
 </div>
+    <br><br>
     <div class="row">
         <div>
             <h3>THANK YOU</h3>
