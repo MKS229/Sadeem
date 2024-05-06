3cf0f2be65b90b19b02c4839d3a6c2370bc679b5
Esraa Alsubhi <162142964+ovzev@users.noreply.github.com>
2024-05-06 19:39:10 +0400

    questionnaire.php

--- questionnaire.php
+++ questionnaire.php
@@ -76,6 +76,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </style>
 </head>
 <body>
+    <div class="container-fluid">
+    <nav class="navbar navbar-expand-sm">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="contactUs.html">Contact us</a></li>
+            <li class="nav-item"><a class="nav-link" href="AboutUs.html">About us</a></li>
+            <li class="nav-item"><a class="nav-link" href="join.html">Join</a></li>
+            <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+            <li class="nav-item"><a class="nav-link" href="store.html">Store</a></li>
+            <li class="nav-item"><a class="nav-link" href="design.html">Design</a></li>
+            <li class="nav-item"><a class="nav-link" href="courses.html">Courses</a></li>
+            <li class="nav-item"><a class="nav-link" href="community.html">Community</a></li>
+            <li class="nav-item"><a class="nav-link" href="sell.html">Sell</a></li>
+            <li class="nav-item"><a class="nav-link" href="cart.html">Cart</a></li>
+        </ul>
+    </nav>
+</div>
+<div class="header">
+    <img src="logo.png" alt="logo" class="logo">
+    <p class="logo">Sadeem</p>
+</div>
     <div class="row">
         <div>
             <h3>THANK YOU</h3>
@@ -85,5 +106,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <?php echo $questionnaire->displayTable(); ?>
         </div>
     </div>
+    <footer>
+    <ul>
+        <li><a href="about.html">About Us</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
+        <li><a href="community.html">Community</a></li>
+    </ul>
+    <div class="footer-icons">
+        <a href="mailto:sadeemartcommunity@gmail.com">
+            <img src="emailicon.png" alt="email icon" style="height: 45px; width: 45px;">
+        </a>
+        <a href="https://github.com/MKS229/Sadeem">
+            <img src="github.png" alt="github icon">
+        </a>
+    </div>
+</footer>
 </body>
 </html>
